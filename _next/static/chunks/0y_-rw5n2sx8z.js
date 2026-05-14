7666c8575cfd59e17ae7713da3b531f506bea8e6
Nandu Kannan M <nandukannanmelath@gmail.com>
2026-05-15 02:26:04 +0530

    deploy: 3D avatar build

--- _next/static/chunks/0y_-rw5n2sx8z.js
+++
@@ -1,1 +0,0 @@
-(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,6179,o=>{o.v(t=>Promise.all(["static/chunks/0nkxor5w806_u.js"].map(t=>o.l(t))).then(()=>t(88627)))}]);
